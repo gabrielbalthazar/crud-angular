03d4b4199307d5fbe801e53d7cd613cfdd4ca882
Gabriel <gabrielbalthazar11@gmail.com>
2026-02-11 11:40:36 -0300

    Ajuste header-footer página

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -16,7 +16,6 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
 
-
 @NgModule({
   declarations: [
     ErrorDialogComponent,
